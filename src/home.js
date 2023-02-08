903961a9f705768796e73710c49bda93c524b7a5
Willis <favorite.ange@gmail.com>
2023-02-08 20:55:29 +0800

    shopcar

--- src/home.js
+++ src/home.js
@@ -6,8 +6,29 @@ export default {
       rwdHeight: {
         height: "",
       },
+      coffieList: [
+        {
+          name: "Classic Coffee Espresso",
+          price: "10",
+          img: "menu-1.png",
+          value: "",
+        },
+        { name: "Machiato", price: "19", img: "menu-2.png", value: "" },
+        { name: "Flat White", price: "15", img: "menu-3.png", value: "" },
+        { name: "Americano", price: "8", img: "menu-4.png", value: "" },
+        {
+          name: "Caramel Macchiato",
+          price: "15",
+          img: "menu-5.png",
+          value: "",
+        },
+        { name: "Mocha", price: "18", img: "menu-6.png", value: "" },
+      ],
+      itemNum: 0,
+      shopcar: [],
       fullWidth: 0,
       fullHeight: 0,
+      sum: 0,
     };
   },
   mounted() {
@@ -33,8 +54,25 @@ export default {
         this.rwdHeight.height = "0px";
       }
     },
-    shopcarShow(){
+    shopcarShow() {
       this.shopcarisShow = !this.shopcarisShow;
-    }
+    },
+    addtoshopcar(i) {
+      // i為該商品的物件
+      this.shopcar.push(i);
+    },
+    // 將商品新增至購物車
+    delshopitem(index) {
+      this.shopcar.splice(index, 1);
+    },
+    //刪除購物車項目
+    shopcartotal() {
+      let sum = 0
+      this.shopcar.map(function (value) {
+        sum += value.price * value.value;
+      });
+      this.sum = sum
+      return this.sum
+    },
   },
 };
